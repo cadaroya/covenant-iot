f47e5ef1cb8bd70fbe4761d54a4f32ac4363e3c1
Carlos Daroya <daroyac@gmail.com>
2018-05-07 21:19:46 +0800

    welcome controller for testing

--- rails-server/app/controllers/api/v1/welcome_controller.rb
+++ rails-server/app/controllers/api/v1/welcome_controller.rb
@@ -2,7 +2,14 @@ module Api
     module V1
         class WelcomeController < ApplicationController
             def index
-                render 'index'
+                @reading = {
+                    "ip_address": "120.0.0.7"
+                    "threshold": 1
+                    "analog_reading": 123
+                }
+
+                render json: {status: 'SUCCESS', data: @reading}
+                    
             end
         end
     end
